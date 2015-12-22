dcb3e5c37f8fa2588254ec9098aaecebd73ac0b4
Karun Siddana <ksiddana@gmail.com>
2015-12-21 21:49:24 -0800

    Fixed the Default Room Name when it is null

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,10 +9,20 @@
 //find a way to post created messages by user to the DOM.
 var app = {};
 
+// Add a property to the application that keeps track of the most recent messages
 app.onscreenMessages = {};
+
+// Add a property to the chatterbox that holds the api endpoint address
 app.server = 'https://api.parse.com/1/classes/chatterbox';
+
+// By default we want the Chatterbox Client to be in the room, selected from the Drop
+// Down Menu
 app.$roomSelect = $('#roomSelect');
+
+// Set the default chatroom to "Lobby"
 app.roomname = 'Lobby';
+
+// Make an Object to hold all the chatrooms.
 app.chatrooms = {};
 
 app.init = function(){
@@ -58,6 +68,7 @@ app.fetch = function(){
         app.populateRooms(data.results)
 
         app.displayMessages(data.results);  
+          
 
     },
     error: function (data) {
@@ -69,29 +80,33 @@ app.fetch = function(){
 };
 
 app.renderMessage = function(message) {
-
-  // debugger
-/*  if (!message.roomname){
-    message.roomname = 'Lobby';
-  }*/
   
+  // A lot of the messages are getting filtered when there's no
+  // chatroom name associated with them. We want to still display
+  // all the messages. 
   if (message.roomname === null) {
-    message.roomname = app.roomname;
+    message.roomname = 'Lobby';
   }
 
+  // We only want to append those messages to the DOM that have
+  // have been selected by the user. message.roomname needs to be valid
+  // for this condition to work, therefore we set the message roomname to 
+  // equal to the approom name.
   if ( message.roomname === app.roomname) {
     var $username = $('<a href="#" class="username">' + message.username +'</a>');
     var $text = $('<div class="text">'+ message.text + '<span class="roomname">' + message.roomname + '</span>' + '</div>');
     var $chat = $('<div>', {class: 'chat', 'data-id': message.objectId, 'data-roomname': message.roomname}).append($username, $text);
   }
 
+  // Return the chat jQuery element from the render message to to the Display
+  // Message.
   return $chat
 }
 
 app.displayMessage = function(message) {
   
   //if (!app.onscreenMessages[message.objectId]) {
-  //if ( message.roomname === roomSelected ) {
+  // if ( message.roomname === roomSelected ) {
       // app.clearMessages();
       var $html = app.renderMessage(message);
 
@@ -117,14 +132,16 @@ app.populateRooms = function(results){
 
   for (var i = 0; i < results.length; i++) {
     var uniqueChatRoomName = results[i].roomname;
-    // console.log(uniqueChatRoomName && !chatrooms[uniqueChatRoomName] || chatrooms[uniqueChatRoomName] === null);
+    
+    // If the chatroom Name is Unique and is not in the app chatroom object, add the 
+    // the room to the app chatroom object and also add it to the drop down menu selection
     if (uniqueChatRoomName && !app.chatrooms[uniqueChatRoomName]) {
       
       // Add the Room to the select menu
       app.addRoom(uniqueChatRoomName);
 
       // Once the chatrooms are fetched from the server, we
-      // want to populate the Drop Down Menu only once.
+      // only want to populate the Drop Down Menu only once.
       app.chatrooms[uniqueChatRoomName] = true;
     }
   }
@@ -132,8 +149,6 @@ app.populateRooms = function(results){
 
 app.addRoom = function(roomname) {
 
-      // console.log(roomname);
-      // Prevent XSS by escaping with DOM methods
       var $option = $('<option value="' + roomname + '">'+ roomname +'</option>');
       // Add to select
       $('#roomSelect').append($option);
@@ -149,9 +164,9 @@ app.changeRoom = function(roomname) {
 var friend = [];
 
 app.addFriend = function(username) {
-  console.log("add Friend is being called it's");
+  
+  // Clicking on a username current inserts the friend into the Friends list.
   friend.push(username);
-  console.log(friend);
 
   for (var i = 0; i < friend.length; i++) {
   //console.log($());
